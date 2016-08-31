6f812207c39db9a177914ad1b1f1016d60dabfb7
ZyanZou <873863981@qq.com>
2016-08-31 17:34:38 +0800

    finish test for setting part

--- test.rb
+++ test.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 require 'mechanize'
-
-
 require 'json'
 require 'base64'
+require_relative 'settings'
+
 
 @agent = Mechanize.new()
 url_personal_information = 'http://weibo.cn/attgroup/opening?uid=%s' % 5235640836
@@ -13,3 +13,6 @@ def parse_personal_information(url)
 end
 
 # parse_personal_information(url_personal_information)
+
+p a = Settings.new().config['BOT_NAME']
+
